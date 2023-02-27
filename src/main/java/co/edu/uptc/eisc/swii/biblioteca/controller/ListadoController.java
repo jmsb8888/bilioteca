22e669e639d17d5cb77252f7289aefa7bbccf2a3
Jose Salamanca <jose.salamanca03@uptc.edu.co>
2023-02-27 17:59:10 -0500

    segundas diapositivas terminadas

--- src/main/java/co/edu/uptc/eisc/swii/biblioteca/controller/ListadoController.java
+++ src/main/java/co/edu/uptc/eisc/swii/biblioteca/controller/ListadoController.java
@@ -1,17 +1,25 @@
 package co.edu.uptc.eisc.swii.biblioteca.controller;
 
+import co.edu.uptc.eisc.swii.biblioteca.domain.Libro;
+import co.edu.uptc.eisc.swii.biblioteca.service.LibroService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.List;
+
 @Controller
 public class ListadoController {
 
+    private final LibroService libroService;
+
+    public ListadoController(LibroService libroService) {
+        this.libroService = libroService;
+    }
 
     @RequestMapping("/")
     public String ListarLibros(Model model) {
-
+        List<Libro> destacados = libroService.buscarDestacados();
+        model.addAttribute("libros", destacados);
         return "listado";
     }
 }
